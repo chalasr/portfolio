bef2e3058a49fb67f969bb94f553ce20b04eb95a
chalasr <robin.chalas@epitech.eu>
2015-03-31 22:14:42 +0200

    upd portfolio

--- src/Robin/PortfolioBundle/Controller/DefaultController.php
+++ src/Robin/PortfolioBundle/Controller/DefaultController.php
@@ -2,17 +2,27 @@
 
 namespace Robin\PortfolioBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
+    public function indexAction($name){
         return $this->render('PortfolioBundle:Default:index.html.twig', array('name' => $name));
     }
 
-    public function homeAction()
-    {
+    public function homeAction(Request $request){
+      if ('POST' === $request->getMethod()){
+        // The http request is of type POST
+        $message = \Swift_Message::newInstance()
+        ->setSubject('Hello Email')
+        ->setFrom('robin.chalas@gmail.com')
+        ->setTo('robin.chalas@epitech.eu')
+        ->setBody($this->renderView('PortfolioBundle:Default:email.html.twig'));
+        $this->get('mailer')->send($message);
+      }
+
         return $this->render('PortfolioBundle:Default:home.html.twig');
     }
 }
