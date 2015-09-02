7e25de4ad5a225054e2d5ddda41d15a1e5dc99f7
Robin Chalas <rchalas@sutunam.com>
2015-09-02 14:56:48 +0200

    fix email

--- src/Robin/PortfolioBundle/Controller/DefaultController.php
+++ src/Robin/PortfolioBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Robin\PortfolioBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,30 +13,48 @@ class DefaultController extends Controller
         return $this->render('PortfolioBundle:Default:index.html.twig', array('name' => $name));
     }
 
-    public function homeAction(Request $request){
-      if ('POST' === $request->getMethod()){
-        // The http request is of type POST
-        $message = \Swift_Message::newInstance()
-        ->setSubject('Hello Email')
-        ->setFrom('robin.chalas@gmail.com')
-        ->setTo('robin.chalas@epitech.eu')
-        ->setBody($this->renderView('PortfolioBundle:Default:email.html.twig'));
-        $this->get('mailer')->send($message);
-      }
+    public function homeAction(Request $request)
+    {
+        if ('POST' === $request->getMethod()){
+            $params = [];
+            $request = Request::createFromGlobals();
+            $fields = ['nom', 'prenom', 'email', 'content'];
+            foreach($fields as $field){
+                $params[$field] = $request->request->get($field);
+            }
+            $mailBody = $this->renderView('PortfolioBundle:Default:email.html.twig', $params);
+
+            $message = \Swift_Message::newInstance()
+              ->setSubject('chalasdev.fr - Contact visiteur')
+              ->setFrom($request->request->get('email'))
+              ->setTo('robin.chalas@gmail.com')
+              ->setBody($mailBody);
+
+            $this->get('mailer')->send($message);
+        }
 
         return $this->render('PortfolioBundle:Default:home-en.html.twig');
     }
 
-    public function homeFrAction(Request $request){
-      if ('POST' === $request->getMethod()){
-        // The http request is of type POST
-        $message = \Swift_Message::newInstance()
-        ->setSubject('Hello Email')
-        ->setFrom('robin.chalas@gmail.com')
-        ->setTo('robin.chalas@epitech.eu')
-        ->setBody($this->renderView('PortfolioBundle:Default:email.html.twig'));
-        $this->get('mailer')->send($message);
-      }
+    public function homeFrAction(Request $request)
+    {
+        if ('POST' === $request->getMethod()){
+            $params = [];
+            $request = Request::createFromGlobals();
+            $fields = ['nom', 'prenom', 'email', 'content'];
+            foreach($fields as $field){
+                $params[$field] = $request->request->get($field);
+            }
+            $mailBody = $this->renderView('PortfolioBundle:Default:email.html.twig', $params);
+
+            $message = \Swift_Message::newInstance()
+              ->setSubject('chalasdev.fr - Contact visiteur')
+              ->setFrom($request->request->get('email'))
+              ->setTo('robin.chalas@gmail.com')
+              ->setBody($mailBody);
+
+            $this->get('mailer')->send($message);
+        }
 
         return $this->render('PortfolioBundle:Default:home.html.twig');
     }
