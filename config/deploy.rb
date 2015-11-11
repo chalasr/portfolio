1ca169b44ba6d094bde3176f9a0c61f664df2f81
chalasr <robin.chalas@epitech.eu>
2015-11-11 13:41:15 +0100

    permissions

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,29 +43,11 @@ set :webserver_user, "www-data"
 set :assets_install_path, fetch(:web_path)
 set :assets_install_flags, '--symlink'
 
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-#
-#
+namespace :deploy do
+  on roles :web do
+    execute :chmod, "-R 777 #{latest_release}/app/cache"
+  end
+end
 # after :clear_cache, :clear_cache do
 #   on roles(:web), wait: 10 do
 #     run "#{sudo} chmod -R 777 /var/www/html/projects/Portfolio/current/app/cache"
