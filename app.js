a6b1a11bc23b748cbdeb955fe4bfc49ee6b6744e
chalasr <robin.chalas@epitech.eu>
2015-12-20 23:27:04 +0100

    add otherwise route

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var http = require('http').Server(app);
 app.use('/assets', express.static('assets'));
 // app.use('/templates', express.static('templates'));
 
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/fr', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/fr', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 http.listen(3000, function(){
   console.log('listening on 127.0.0.1:3000');
